c9cdf15a4ba9ace660b2b4403bb557b43fd6fd3d
Nischay Sharma <nischay.sharma@edvanta.com>
2024-01-27 16:02:42 +0530

    Add sprite class

--- index.js
+++ index.js
@@ -14,23 +14,95 @@ backgroundImage.src = './img/Pellet Town.png';
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
-console.log(backgroundImage);
+// console.log(backgroundImage);
 
 backgroundImage.onload = () => {
-  c.drawImage(backgroundImage, -740, -600);
   console.log("Pallet Town image loaded...");
-  playerImage.onload = () => {
+}
+playerImage.onload = () => {
+  console.log("player image loaded...")
+}
+
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position
+    this.image = image
+  }
+  draw() {
+
     c.drawImage(
-      playerImage,
-      0,
-      0,
-      playerImage.width / 4,
-      playerImage.height,
-      canvas.width / 2 - playerImage.width / 8,
-      canvas.height / 2 - playerImage.height / 2,
-      playerImage.width / 4,
-      playerImage.height
-    )
-    console.log("player image loaded...")
+      this.image,
+      this.position.x,
+      this.position.y
+    );
   }
 }
+
+const background = new Sprite({
+  position: {
+    x: -740,
+    y: -600
+  },
+  image: backgroundImage
+})
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  // console.log("Animate...")
+  background.draw()
+
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 8,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  )
+}
+animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true
+      console.log("A key is pressed..", event.code)
+      break;
+
+    case 'KeyA':
+      keys.a.pressed = true
+      console.log("A key is pressed..", event.code)
+      break;
+
+    case 'KeyS':
+      keys.s.pressed = true
+      console.log("A key is pressed..", event.code)
+      break;
+
+    case 'KeyD':
+      keys.d.pressed = true
+      console.log("A key is pressed..", event.code)
+      break;
+
+    default:
+      break;
+  }
+})
