c9765e9033ccd650cb8df9383cb39c0f2b3e7817
Nischay Sharma <nischay.sharma@edvanta.com>
2024-01-27 13:48:59 +0530

    adding all images

--- TiledAssets/Pokemon Style Game.tsx
+++ TiledAssets/Pokemon Style Game.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Pokemon Style Game" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
  <image source="../Assets/First Asset pack.png" width="384" height="384"/>
+ <tile id="126">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="11.9941" height="11.9447"/>
+  </objectgroup>
+ </tile>
 </tileset>
