0421cebc4868741cc0b92cc6eea29081b2c9ae4d
Nischay Sharma <nischay.sharma@edvanta.com>
2024-01-27 14:17:21 +0530

    Add Town map on canvas

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ canvas.width = 1024;
 canvas.height = 576;
 
 // console.log(c)
-c.fillStyle = 'white'
+c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const backgroundImage = new Image()
-backgroundImage.src = './img/Pellet Town.png'
-// c.drawImage
-console.log(backgroundImage)
+const backgroundImage = new Image();
+backgroundImage.src = './img/Pellet Town.png';
+console.log(backgroundImage);
+
+backgroundImage.onload = () => {
+  c.drawImage(backgroundImage, -750, -550);
+  console.log("Pallet Town image loaded...");
+}
